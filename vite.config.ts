77149e8b94bec555a81ab355deb750e54bd447c6
Kazaneza Gentil <kazanezagentil@gmail.com>
2025-10-02 10:17:10 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    outDir: 'dist',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['react', 'react-dom', 'react-router-dom'],
   },
   server: {
     host: '0.0.0.0',
