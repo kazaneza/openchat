ff1620b58afdddd7720fd2c8fe1e6b394fd2c9ab
Gentil Kazaneza <kazanezagentil@gmail.com>
2025-11-10 09:54:10 +0200

    fixed the error

---
+++ backend/models/feedback.py
@@ -0,0 +1,6 @@
+# backend/models/feedback.py
+
+class FeedbackModel:
+    """Placeholder model to satisfy imports. Expand if needed later."""
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
